9dcba6ebfba8f57feae45c4c0170ec429c568ca7
vhok74 <wogh8732@naver.com>
2020-06-21 17:27:38 +0900

    delete elcass folder

--- eclass/include/nav.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-	<nav id="Nav-Menu">
-		<ul>
-			<li><a class="menubar" href="/index.php">메인</a></li>
-			<li><a class="menubar" href="/page/evaluation.php">강의평가</a></li>
-			<li><a class="menubar" href="/page/login.php">회원정보</a></li>
-		</ul>
-	</nav> 
-</div>
